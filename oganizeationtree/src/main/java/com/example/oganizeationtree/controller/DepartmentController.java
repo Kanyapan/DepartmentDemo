aab11970ddaaee64d2bee222a93ea3e1905d5f35
kanyapan.v <kanyapan.v@iconext.co.th>
2023-08-27 16:23:12 +0700

    update

--- oganizeationtree/src/main/java/com/example/oganizeationtree/controller/DepartmentController.java
+++ oganizeationtree/src/main/java/com/example/oganizeationtree/controller/DepartmentController.java
@@ -2,32 +2,62 @@ package com.example.oganizeationtree.controller;
 
 import com.example.oganizeationtree.entity.Department;
 import com.example.oganizeationtree.service.DepartmentService;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.ByteArrayOutputStream;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+
 public class DepartmentController {
 
     @Autowired
     private DepartmentService departmentservice;
 
 
-    @GetMapping("/departments")
+    @GetMapping("/getDepartments")
     public ResponseEntity<List<Department>> findAll() {
-        List<Department> list= departmentservice.findAll();
+        List<Department> list = departmentservice.findAll();
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
-    @GetMapping("/departmentName")
+
+    @GetMapping("/getDepartmentName")
     public ResponseEntity<Department> findById(@RequestParam String id) {
-        Department department= departmentservice.findByDepartmentId(id);
+        Department department = departmentservice.findByDepartmentId(id);
         return new ResponseEntity<>(department, HttpStatus.OK);
     }
+
+    @GetMapping("/generate")
+    public ResponseEntity<ByteArrayResource> generateExcel() {
+        List<Department> dataList = departmentservice.findAll(); // Replace with your data retrieval logic
+
+        Workbook workbook = departmentservice.generateExcel(dataList);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
+        headers.setContentDispositionFormData("attachment", "data_export.xlsx");
+
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
+            workbook.write(byteArrayOutputStream);
+            ByteArrayResource resource = new ByteArrayResource(byteArrayOutputStream.toByteArray());
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentLength(resource.contentLength())
+                    .body(resource);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
+
--- oganizeationtree/src/main/java/com/example/oganizeationtree/service/DepartmentService.java
+++ oganizeationtree/src/main/java/com/example/oganizeationtree/service/DepartmentService.java
@@ -4,9 +4,12 @@ import com.example.oganizeationtree.entity.Department;
 import com.example.oganizeationtree.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class DepartmentService {
@@ -21,7 +24,31 @@ public class DepartmentService {
 
     public Department findByDepartmentId(String id) {
         return repository.findByDepartmentId(id);
-
     }
+
+    public Workbook generateExcel(List<Department> dataList) {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Data");
+        List<Department> departmentList = repository.findAll();
+        // Create header row
+        Row headerRow = sheet.createRow(0);
+        headerRow.createCell(0).setCellValue("department_id");
+        headerRow.createCell(1).setCellValue("department_name");
+        headerRow.createCell(2).setCellValue("org_code");
+        // Add more headers as needed
+
+        // Populate the sheet with data
+        int rowNum = 1;
+        for (Department department : departmentList) {
+            Row row = sheet.createRow(rowNum++);
+            row.createCell(0).setCellValue(department.getDepartmentId());
+            row.createCell(1).setCellValue(department.getDepartmentName());
+            row.createCell(2).setCellValue(department.getOrgCode());
+            // Add more cell values as needed
+        }
+
+        return workbook;
+    }
+
 }
 
